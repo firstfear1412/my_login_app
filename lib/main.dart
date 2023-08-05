55ea9ad6013950967e04cb826413e71bb3dbab28
firstfear1412 <78691900+firstfear1412@users.noreply.github.com>
2023-08-05 23:49:14 +0700

    move path and fix some problem of sign up

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:my_login_app/pages/auth_page.dart';
-import 'package:my_login_app/pages/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
